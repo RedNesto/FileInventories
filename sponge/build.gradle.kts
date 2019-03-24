f097f81e653c7005e8eb4e58dc4e90aa9d4f276e
RedNesto <RedNestoMC@gmail.com>
2019-03-24 11:13:23 +0100

    Add JitPack setup for the Sponge module Bump to Gradle 5.3

--- sponge/build.gradle.kts
+++ sponge/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("maven")
     id("org.spongepowered.plugin") version "0.9.0"
 }
 
@@ -12,4 +13,23 @@ sponge.plugin.id = extra["pluginId"] as String
 
 tasks.withType<Jar> {
     baseName = "FileInventories-Sponge"
+}
+
+val sourcesJar = tasks.create("sourcesJar", Jar::class) {
+    dependsOn("classes")
+    classifier = "sources"
+    afterEvaluate {
+        from(the<SourceSetContainer>()["main"].allSource)
+    }
+}
+
+val javadocJar = tasks.create("javadocJar", Jar::class) {
+    dependsOn("javadoc")
+    classifier = "javadoc"
+    from(tasks.getByName("javadoc", Javadoc::class).destinationDir)
+}
+
+artifacts {
+    add("compile", sourcesJar)
+    add("compile", javadocJar)
 }
